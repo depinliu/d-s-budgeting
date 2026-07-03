49e3936ae66dc7e4157b2e279af5daa55e2f626f
gorosei <devin@hasnurgroup.com>
2026-07-03 08:47:42 +0700

    Add new icons and update service worker and HTML references
    - Introduced new icons: EyeOffIcon, EyeIcon, LockIcon, MailIcon, PiggyBankIcon, RepeatIcon, ScanLineIcon, SendIcon, ShieldIcon, SmartphoneIcon, SparklesIcon, TargetIcon, TrendingUpIcon, UsersIcon, WalletMinimalIcon, and XIcon.
    - Updated the index.html to reference the new JavaScript file index-GvEdnV80.js.
    - Modified the service worker (sw.js) to include new asset revisions and ensure proper caching.

--- assets/send-CFVlWQyx.js
+++
@@ -1,6 +0,0 @@
-import{K as e}from"./index-CJIQplui.js";/**
- * @license lucide-vue-next v0.372.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const o=e("SendIcon",[["path",{d:"m22 2-7 20-4-9-9-4Z",key:"1q3vgg"}],["path",{d:"M22 2 11 13",key:"nzbqef"}]]);export{o as S};
